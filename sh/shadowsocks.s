8a313f155593b618656663288bc4b86b0d24d923
levelip <levelip@users.noreply.github.com>
2016-05-28 10:55:10 +0800

    Update shadowsocks.s

--- sh/shadowsocks.s
+++ sh/shadowsocks.s
@@ -2,6 +2,7 @@ wget -c  http://soli-10006287.cos.myqcloud.com/oneinstack.tar.gz
 tar xzf oneinstack.tar.gz
 cd oneinstack
 ./shadowsocks.sh install
+chkconfig --add shadowsocks
 wget  http://fs.d1sm.net/finalspeed/install_fs.sh
 chmod +x install_fs.sh
 ./install_fs.sh 2>&1 | tee install.log
