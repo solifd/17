104b3c3de05a90a0af9ff070a0a7be4524064333
levelip <levelip@users.noreply.github.com>
2016-08-09 21:05:03 +0800

    Update shadowsocks-libev.sh

--- sh/shadowsocks-libev.sh
+++ sh/shadowsocks-libev.sh
@@ -7,3 +7,10 @@ make && make install
 #nohup /usr/local/bin/ss-server -s 156.132.67.213 -p 8981 -k admin888 -m aes-256-cfb &
 #echo "nohup /usr/local/bin/ss-server -s IP地址 -p 端口 -k 密码 -m 加密方式 &" >> /etc/rc.local
 #echo "sh /root/ss.sraer" >> /etc/rc.local
+curl "https://raw.githubusercontent.com/lj2007331/oneinstack/master/init.d/Shadowsocks-libev-init " -o  /etc/init.d/shadowsocks
+chmod +x /etc/init.d/shadowsocks
+chkconfig --add shadowsocks
+chkconfig shadowsocks on
+
+mkdir /etc/shadowsocks
+curl "https://raw.githubusercontent.com/solifd/ph/master/config.json" -o  /etc/shadowsocks/config.json
